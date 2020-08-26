c8cefd32afbcaf4a3d9aaaa13470bbb95a49dbad
Yoskele <yossi.kayhko@gmail.com>
2020-08-26 10:10:22 +0300

    STATIC_ROOT2

--- backend/settings.py
+++ backend/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/yosefLotter/djangoWebpack/backend/static/'
+STATIC_ROOT = '/home/yosefLotter/djangoWebpack/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
