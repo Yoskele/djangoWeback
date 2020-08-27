0ddaf0c3e819298d924727caeed4f19260a02d34
Yoskele <yossi.kayhko@gmail.com>
2020-08-27 10:41:06 +0300

    imageTest

--- backend/urls.py
+++ backend/urls.py
@@ -9,11 +9,13 @@ router = routers.DefaultRouter()
 # register the links how you want it to start. and the function that comes from views.py file
 router.register('articles', ArticleViewSet, basename="articles"),
 
-from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('', views.index),
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    re_path('', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
