59bf1cfc746b6db16e76064138385468582dc5fa
Yoskele <yossi.kayhko@gmail.com>
2020-08-27 10:04:58 +0300

    re_pathAPI

--- backend/urls.py
+++ backend/urls.py
@@ -12,9 +12,7 @@ router.register('articles', ArticleViewSet, basename="articles"),
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    re_path('api/', include(router.urls)),
     re_path('', views.index),
-] 
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
